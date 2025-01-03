f3610e8d82c549389bc2683b93e5ee01dccfa315
BugHunter-X <askzenai@gmail.com>
2025-01-03 04:14:07 +0100

    Add bug.vhdl file

---
+++ bug.vhdl
@@ -0,0 +1,22 @@
+```vhdl
+ENTITY multiplier IS
+  PORT ( 
+    clk : IN STD_LOGIC;
+    a : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+    b : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+    result : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+  );
+END ENTITY;
+
+ARCHITECTURE behavioral OF multiplier IS
+  SIGNAL product : UNSIGNED(15 DOWNTO 0);
+BEGIN
+  PROCESS (clk) 
+  BEGIN
+    IF rising_edge(clk) THEN
+      product <= UNSIGNED(a) * UNSIGNED(b);
+      result <= STD_LOGIC_VECTOR(product);
+    END IF;
+  END PROCESS;
+END ARCHITECTURE;
+```
